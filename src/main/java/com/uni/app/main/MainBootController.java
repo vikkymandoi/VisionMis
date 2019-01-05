d365894b568bd730f14255e16870e817443bdf2b
vikkymandloi <imvrajendra@gmail.com>
2019-01-05 05:38:13 +0000

    Fix for Project to be without Spring

--- src/main/java/com/uni/app/main/MainBootController.java
+++ src/main/java/com/uni/app/main/MainBootController.java
@@ -1,11 +1,7 @@
 package com.uni.app.main;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class MainBootController {
 	public static void main(String[] args) {
-		SpringApplication.run(MainBootController.class, args);
+		System.out.println("Good Morning");
 	}
 }
